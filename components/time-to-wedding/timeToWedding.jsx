e6a65630f2193e3fd20a5e69ce2506e60b733010
Archimedes Fagundes Junior <archimedes.junior@nava.com.br>
2022-02-11 00:07:16 -0300

    refatoração dos nomes dos componentes

---
+++ components/time-to-wedding/timeToWedding.jsx
@@ -0,0 +1,32 @@
+import styles from './timeToWedding.module.css'
+import Timer from '../timer/timer'
+
+export default function TimeToWedding() {
+    const eventDate = '10-07-2022 15:00:00';
+
+    return (
+        <section className={styles.timeToWedding}>
+            <div className={styles.container}>
+                <h2>O Grande Dia<br />10 . Julho . 2022</h2>
+
+                <div className={styles.messageContainer}>
+                    <p className={styles.message}>
+                        Em breve vamos celebrar o amor, a amizade e a união do casal mais 
+                        apaixonado que vocês já conheceram.
+                    </p>
+                    <p className={styles.message}>
+                        Contamos com a presença de todos.
+                    </p>
+                </div>
+                
+                <div className={styles.timerContainer}>
+                    Espera mais um pouco... 
+
+                    <Timer eventDate={eventDate}></Timer>
+                    
+                    ...para gente curtir a festa!
+                </div>
+            </div>
+        </section>
+    )
+}
