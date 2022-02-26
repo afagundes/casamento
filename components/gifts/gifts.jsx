fcb5d714122a492f7779355a16567833187b8e46
Archimedes Fagundes Junior <archimedes.fagundes@gmail.com>
2022-02-26 11:41:33 -0300

    texto da lista de presentes

--- components/gifts/gifts.jsx
+++ components/gifts/gifts.jsx
@@ -1,15 +1,27 @@
-import style from './gifts.module.css';
+import styles from './gifts.module.css';
 
 export default function Gifts() {
     return (
-        <section className={`${style.gifts} container_gray`}>
+        <section className={`${styles.gifts} container_gray`}>
             <h2>Lista de Presentes</h2>
 
-            <p>Essa é nossa lista de presentes. blablablablabla</p>
-
-            <div className={style.giftGrid}>
-
+            <div className={styles.message}>
+                <p>Amigos e familiares,</p>
+                <p>Fizemos essa lista como uma sugestão caso vocês queiram nos presentear.</p>
+                <p>
+                    Como sabem, nossa casa já está pronta. Portanto, ao invés de itens materiais preferimos ser
+                    presenteados com momentos.
+                </p>
+                <p>Optamos por pagamento PIX por ser mais simples, mas fiquem à vontade para nos dar dinheiro de qualquer outra forma 😀</p>
+                <p>(Claro, essa lista é apenas para se divertirem. Fiquem à vontade para escolherem os valores.)</p>
             </div>
+
+            <div className={styles.giftGrid}>
+                Presentes
+            </div>
+
+            <p>Informação do PIX</p>
+
         </section>
     );
 }
